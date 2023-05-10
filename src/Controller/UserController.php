113c4b2df599244635e513264037c53a001f9861
tz7 <domain.rlstr@tutanota.com>
2023-05-10 20:23:34 +0200

    Password display fix in admin/user

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -26,18 +26,27 @@ class UserController extends AbstractController
     #[Route('/', name: 'app_user_index', methods: ['GET'])]
     public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
+        foreach ($users as $user) {
+            $user->setPassword('');
+        }
+
         return $this->render('user/index.html.twig', [
-            'users' => $userRepository->findAll(),
+            'users' => $users,
         ]);
     }
 
     #[Route('/{id}', name: 'app_user_show', methods: ['GET'])]
     public function show(User $user): Response
     {
+        $user->setPassword('');
+
         return $this->render('user/show.html.twig', [
             'user' => $user,
         ]);
     }
+
 
     #[Route('/{id}/edit', name: 'app_user_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, User $user, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
